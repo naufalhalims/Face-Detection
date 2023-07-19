c2330aae8065eab09aefe151bcd27b2f97ad7908
ardhiantor <81969871+ardhiantor@users.noreply.github.com>
2023-07-19 19:21:20 +0700

    First Push

---
+++ Face_Detection.py
@@ -0,0 +1,38 @@
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Capture video from the webcam
+video_capture = cv2.VideoCapture(2)
+
+while True:
+    # Read each frame
+    _, frame = video_capture.read()
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    # Draw rectangles and labels around the faces
+    for (x, y, w, h) in faces:
+        # Draw rectangle around the face
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        # Add text label
+        cv2.putText(frame, 'Wajah', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+
+    # Display the output
+    cv2.imshow('Face Detection', frame)
+
+    # Quit if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the VideoCapture object
+video_capture.release()
+
+# Close all OpenCV windows
+cv2.destroyAllWindows()
